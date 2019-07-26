1dd9fc15efb453b61a283f1fb72e9f9d914a5c1b
Arthur <arthurcastroes@gmail.com>
2019-07-26 20:03:39 -0300

    initial files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const expressLayout = require("express-ejs-layouts");
+const indexController = require("./routes/index");
+const mongoose = require("mongoose");
+
+const url = process.env.DATABASE_URL || "mongodb://localhost/mybrary";
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayout);
+app.use(express.static("public"));
+
+app.use("/", indexController);
+
+//DATABASE
+mongoose.connect(url, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log("Connected to MongoDB..."));
+
+app.listen(process.env.PORT || 3000, console.log("Listening on PORT 3000..."));
